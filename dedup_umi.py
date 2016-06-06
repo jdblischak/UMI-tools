8d979ff840b3c08457a7cbad6bd85d356f52f879
John Blischak <jdblischak@gmail.com>
2016-06-06 14:22:39 -0500

    Explicitly specify python2 in shebang line so these scripts can also be run in an environment where python3 is the main python executable.

--- dedup_umi.py
+++ dedup_umi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 '''
 DedupUMI.py - Deduplicate reads that are coded with a UMI
--- extract_umi.py
+++ extract_umi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 '''
 cgat_script_template.py - template for CGAT scripts
